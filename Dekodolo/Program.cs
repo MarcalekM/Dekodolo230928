c9eb9e46d0cf97bae518dfa6f8d61d48bc96e784
MarcalekM <120452154+MarcalekM@users.noreply.github.com>
2023-09-28 10:19:53 +0200

    Add files via upload

---
+++ Dekodolo/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Encodings;
+
+namespace Dekodolo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var karakterek = new List<Karakter>();
+            using var sr = new StreamReader(
+                @"..\..\..\src\bank.txt",
+                System.Text.Encoding.UTF8);
+            while (!sr.EndOfStream)
+            {
+                char b = char.Parse(sr.ReadLine());
+                bool[,] m = new bool[7, 4];
+                for (int s = 0; s < 7; s++)
+                {
+                    string sor = sr.ReadLine();
+                    for (int o = 0; o < sor.Length; o++)
+                    {
+                        m[s, o] = sor[o] == '1' ? true : false;
+                    }
+                }
+                karakterek.Add(new Karakter(b, m));
+            }
+            Console.WriteLine($"Karakterek száma: {karakterek.Count} db");
+
+            char input = '\0';
+            do
+            {
+                Console.Write("Input:  ");
+                input = char.Parse(Console.ReadLine());
+            } while (input < 65 || input > 90);
+        }
+    }
+}
