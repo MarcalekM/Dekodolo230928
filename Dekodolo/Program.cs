6945b35558631d33836ae313bb533c23467de20a
MarcalekM <120452154+MarcalekM@users.noreply.github.com>
2023-10-03 22:01:02 +0200

    Update Program.cs

--- Dekodolo/Program.cs
+++ Dekodolo/Program.cs
@@ -11,19 +11,30 @@ namespace Dekodolo
         static void Main(string[] args)
         {
             var bank = Beolvas(@"..\..\..\src\bank.txt");
-            Console.WriteLine($"Karakterek száma: {bank.Count} db");
+
+            Console.WriteLine($"karakterek szama: {bank.Count}");
 
             char input = '\0';
             bool res = false;
             do
             {
-                Console.Write("Input:  ");
+                Console.Write("input: ");
                 res = char.TryParse(Console.ReadLine(), out input);
-            } while (!res || (input < 65 || input > 90));
+            } while (!res || input < 65 || input > 90);
 
-            var mgj = bank.SingleOrDefault(k => k.Betu == input);
-            if (mgj is not null) Console.Write(mgj.Kirajzol());
-            else Console.WriteLine("Nincs ilyen a bankban");
+            var megj = bank.SingleOrDefault(k => k.Betu == input);
+            if (megj is not null) Console.Write(megj.Kirajzol());
+            else Console.WriteLine("nincs ilyen a bankban");
+
+            var dekodolando = Beolvas(@"..\..\..\src\dekodol.txt");
+
+            Console.Write("dekodolas: ");
+            foreach (var dekk in dekodolando)
+            {
+                var bankk = bank.SingleOrDefault(k => k.Felismer(dekk));
+                Console.Write(bankk is null ? "?" : bankk.Betu);
+            }
+            Console.Write('\n');
         }
 
         static List<Karakter> Beolvas(string eleresiUt)
